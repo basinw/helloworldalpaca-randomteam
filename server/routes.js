be50beb5f5835e1f4c0db478583ae755505339e1
ImagineRabbits <mixxdarkz2@gmail.com>
2017-10-09 02:09:29 +0700

    fix reset , add setall , add use key

--- server/routes.js
+++ server/routes.js
@@ -8,8 +8,9 @@ router.route('/id/reset').post(IdController.resetId)
 router.route('/id').post(IdController.getIdById)
 
 router.route('/team').get(TeamController.getAll)
+router.route('/team/reset').post(TeamController.resetTeam)
+router.route('/team/set').post(TeamController.setAll)
 router.route('/team/:team').post(TeamController.addTeam)
 router.route('/team/:team').put(TeamController.pushTeam)
-router.route('/team/reset').post(TeamController.reset)
 
 module.exports = router

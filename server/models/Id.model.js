b1a3637a348c6e5a7d19d4552e0741f18b322e5a
basinw <bas10600@gmail.com>
2017-10-08 21:30:23 +0700

    Resurrection Id.model

---
+++ server/models/Id.model.js
@@ -0,0 +1,45 @@
+// init ID
+let ID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const _BACKUP = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let getId = args => {
+  return ID[args]
+}
+
+let getAll = () => {
+  return ID
+}
+
+module.exports = {
+  reset: () => {
+    return new Promise( async (res, rej) => {
+      try {
+        ID = _BACKUP
+        let allId = await getAll()
+        res(allId)
+      } catch (error) {
+        rej(error)
+      }
+    })
+  },
+  getOne: args => {
+    return new Promise( async (res, rej) => {
+      try {
+        let id = await getId(args)
+        res(id)
+      } catch (error) {
+        rej(error)
+      }
+    })
+  },
+  getAll: () => {
+    return new Promise( async (res, rej) => {
+      try {
+        let id = ID
+        res(ID)
+      } catch (error) {
+        rej(error)
+      }
+    })
+  }
+}

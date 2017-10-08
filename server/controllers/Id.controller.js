86b93022ddacd77ebd6c6f8c51b31b57ad017f35
ImagineRabbits <mixxdarkz2@gmail.com>
2017-10-08 16:43:54 +0700

    add members when random complete and set limit by team

--- server/controllers/Id.controller.js
+++ server/controllers/Id.controller.js
@@ -1,20 +1,38 @@
 const Id = require('../models/Id.model')
 
-<<<<<<< HEAD
-let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-const _BACKUP = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-=======
 // exception index 0
 let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
->>>>>>> master
-let members = []
-team = ["Front-End","Design","Game","Infra"]
+
+const _BACKUP = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+// LIMIT MEMBERS
+let _LIMIT = 3
+let members = ['default']
+
+const team = ['FrontEnd', 'Design', 'Infra']
+
+let setLimit = (team) => {
+  switch (team) {
+    case 'FrontEnd':
+      _LIMIT = 3;
+      break;
+    case 'Design':
+      _LIMIT = 1;
+    case 'Infra':
+      _LIMIT = 1;
+      break;
+    default:
+      _LIMIT = 3;
+      break;
+  }
+}
 
 module.exports = {
   resetId: async (req, res) => {
-    let allId
+    let allId, getCount
     if (req.body.key==='hellofrontend') {
       allId = await Id.reset()
+      getCount = count
       count = _BACKUP
     } else {
       res.json({
@@ -24,16 +42,27 @@ module.exports = {
     }
     res.json({
       status: true,
-      id: allId
+      id: allId,
+      count: getCount
     })
   },
   getIdById: async (req, res) => {
-    let id = +req.params.id
     let resolveId
-    if (count[id]<3) {
+    let id = await +req.params.id
+    let team = await req.body.team
+
+    await setLimit(team)
+
+    if (count[id]<_LIMIT) {
       count[id]++
       resolveId = await Id.getOne(id-1)
-      members[id-1].push(req.body.name)
+      
+      if (members[id]!=undefined) {
+        members[id].push(req.body.name)
+      } else {
+        members[id] = [req.body.name];
+      }
+
     } else {
       count[0]++
       resolveId = 0
@@ -50,7 +79,6 @@ module.exports = {
         id: resolveId
       })
     }
-
   },
   getAll: async (req, res) => {
     let id = await Id.getAll()
